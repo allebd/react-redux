99ab1ae3a766312c91f7b72aebea674f8fa00c1d
Bella Oyedele <delebella@yahoo.com>
2019-09-16 05:39:20 +0100

    Add sample files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ['@babel/react', '@babel/env']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
